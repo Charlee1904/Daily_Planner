a8a1b87bc26b881d62413a6f5d27d44c86721396
Charlee <Charleslee1904@outlook.com>
2021-03-08 23:19:58 -0800

    adding main files HTML/CSS/JS

---
+++ assets/script.js
@@ -0,0 +1,191 @@
+var lcTime = moment();
+
+
+$('document').ready(function() {
+
+//date on the jumbotron
+var curDay = moment().format('MMMM Do YYYY, h:mm a');
+$("#currentDay").text(curDay);
+
+
+//change to input box
+$("input").css("height","75px");
+$("input").css("background-color", "lawngreen",);
+
+
+//adding business standard time
+var nine = moment().hour(9).format('h a');
+$("#nine").text(nine);
+
+var ten = moment().hour(10).format('H a');
+$("#ten").text(ten);
+
+var eleven = moment().hour(11).format('h a');
+$("#eleven").text(eleven);
+
+var noon = moment().hour(12).format('h a');
+$("#noon").text(noon);
+
+var one = moment().hour(13).format('h a');
+$("#one").text(one);
+
+var two = moment().hour(14).format('h a');
+$("#two").text(two);
+
+var three = moment().hour(15).format('h a');
+$("#three").text(three);
+
+var four = moment().hour(16).format('h a');
+$("#four").text(four);
+
+var five = moment().hour(17).format('h a');
+$("#five").text(five);
+
+//background color changes based on past, present, or future.
+if (moment().hour(23).isBefore(lcTime)){
+    $('#tc9').css('background','cornflowerblue');
+
+    } else if (moment().hour(23).isSame(lcTime)){
+        $('#tc9').css('background','blue')
+    };
+
+if (moment().hour(10).isSameOrBefore(lcTime)){
+     $('#tc10').css('background','cornflowerblue');
+    };
+
+ if (moment().hour(11).isSameOrBefore(lcTime)){
+    $('#tc11').css('background','cornflowerblue');
+    };
+
+if (moment().hour(12).isSameOrBefore(lcTime)){
+    $('#tc12').css('background','cornflowerblue');
+    };
+           
+
+if (moment().hour(13).isSameOrBefore(lcTime)){
+    $('#tc13').css('background','cornflowerblue');
+     };
+  
+if (moment().hour(14).isSameOrBefore(lcTime)){
+       $('#tc14').css('background','cornflowerblue');
+     };
+
+
+if (moment().hour(15).isSameOrBefore(lcTime)){
+    $('#tc15').css('background','cornflowerblue');
+     };
+                   
+if (moment().hour(16).isSameOrBefore(lcTime)){
+     $('#tc16').css('background','cornflowerblue');
+ 
+};
+
+ if (moment().hour(17).isSameOrBefore(lcTime)){
+     $('#tc17').css('background','cornflowerblue');
+     };
+
+ });
+//the input gets turned into a var when button is pushed and added to local storage.
+function saveInfo9(){
+    var hr9 = document.getElementById('tc9').value;
+    localStorage.setItem('hour-9',hr9)}
+
+function saveInfo10(){
+    var hr10 = document.getElementById('tc10').value;
+    localStorage.setItem('hour-10',hr10);}
+
+function saveInfo11(){
+    var hr11 = document.getElementById('tc11').value;
+    localStorage.setItem('hour-11',hr11);}
+
+function saveInfo12(){
+    var hr12 = document.getElementById('tc12').value;
+    localStorage.setItem('hour-12',hr12);}
+
+function saveInfo13(){
+    var hr13 = document.getElementById('tc13').value;
+    localStorage.setItem('hour-13',hr13);}
+
+ function saveInfo14(){   var hr14 = document.getElementById('tc14').value;
+    localStorage.setItem('hour-14',hr14);}
+
+function saveInfo15(){
+    var hr15 = document.getElementById('tc15').value;
+    localStorage.setItem('hour-15',hr15);}
+function saveInfo16(){
+    var hr16 = document.getElementById('tc16').value;
+    localStorage.setItem('hour-16',hr16);}
+
+function saveInfo17(){
+    var hr17 = document.getElementById('tc17').value;
+    localStorage.setItem('hour-17',hr17);  
+
+};
+//turning local storage val into a var to 
+var ls9 = localStorage.getItem("hour-9");
+var   ls10 = localStorage.getItem("hour-10");
+var   ls11 = localStorage.getItem("hour-11");
+var   ls12 = localStorage.getItem("hour-12");
+var   ls13 = localStorage.getItem("hour-13");
+var   ls14 = localStorage.getItem("hour-14");
+var   ls15 = localStorage.getItem("hour-15");
+var   ls16 = localStorage.getItem("hour-16");
+var   ls17 = localStorage.getItem("hour-17");
+
+//Stored input 
+$('#tc9').val(ls9);
+$('#tc10').val(ls10);
+$('#tc11').val(ls11);
+$('#tc12').val(ls12);
+$('#tc13').val(ls13);
+$('#tc14').val(ls14);
+$('#tc15').val(ls15);
+$('#tc16').val(ls16);
+$('#tc17').val(ls17);
+
+
+
+// if the time is before local time is after blocktime turn background grey
+//if its current time turn light blue 
+//iff its in the future turn green.
+
+
+
+
+// var lcTime = moment().get('hour');
+// if(lcTime.isBefore(blockTime)){
+//     $('tc9').css('background','red');
+// }
+
+//     $(function () {
+
+//         $("#submit").click(function () {
+//             var name = $("#name").val();
+//             alert(name);cd
+//             localStorage.setItem('username',name);
+//         });
+//     });
+// }); 
+// var lcTime = moment().format('h a');
+// if the time is < than current time - box turns grey
+//if the time is === to current time - box turns red
+//if the time is > than current time - box turns green
+
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+
+
+
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
+
+
+ 
